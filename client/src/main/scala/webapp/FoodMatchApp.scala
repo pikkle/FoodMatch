75d19bccac38c54bde14fcee9756ffe17b0a923f
Loïc Serafin <loicserafin@gmail.com>
2017-06-11 11:29:23 +0200

    clean interface, keyboard shortcuts

--- client/src/main/scala/webapp/FoodMatchApp.scala
+++ client/src/main/scala/webapp/FoodMatchApp.scala
@@ -7,16 +7,29 @@ import scala.scalajs.js.annotation.JSExportTopLevel
 
 object FoodMatchApp extends JSApp {
 	def main(): Unit = {
-		dom.document.getElementById("submit").addEventListener("keyPressed", callback)
+		dom.document.addEventListener("keypress", { (e0: dom.Event) =>
+			val e = e0.asInstanceOf[dom.KeyboardEvent]
+			e.key match {
+				case "a" => chooseLeft()
+				case "l" => chooseRight()
+				case _ =>
+			}
+		}, false)
 	}
 
-	def callback(): Unit = {
-
+	def postChoice(choice: String): Unit = {
+		val ajax = new dom.XMLHttpRequest()
+		ajax.open("GET", "https://jsonplaceholder.typicode.com/posts/1")
+		ajax.onload = (e: dom.Event) => {
+			if (ajax.status == 200) println(ajax.responseText) else println(s"ERROR WHILE FETCHING: $ajax.status")
+		}
+		ajax.send()
 	}
 
 	@JSExportTopLevel("chooseLeft")
 	def chooseLeft(): Unit = {
 		println("You voted for left")
+		postChoice("")
 	}
 
 	@JSExportTopLevel("chooseRight")
