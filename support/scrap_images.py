e0bcb01d7fdb6bdf8bfd8ced190cb3c630f4d7f8
Julien Leroy <julien.leroy@limayankee.com>
2017-06-12 16:40:09 +0200

    update py scipt

--- support/scrap_images.py
+++ support/scrap_images.py
@@ -9,9 +9,10 @@ dishes = []
 
 class Dish:
 
-    def __init__(self, name, img_url):
+    def __init__(self, name, img_url, keywords):
         self.name = name
         self.img_url = img_url
+        self.keywords = keywords
 
     def __str__(self):
         return "Name: " + self.name + " image: " + self.img_url
@@ -41,13 +42,13 @@ class Dish:
 #         count += 1
 
 
-db = MySQLdb.Connect(host="remote.limayankee.com", user="foodmatch", passwd="foodmatch2017", db="foodmatch")
+db = MySQLdb.Connect(host="remote.limayankee.com", user="foodmatch", passwd=PASSWORD, db="foodmatch")
 
 
 def add_dish_to_db(dish):
     try:
         c = db.cursor()
-        c.execute("INSERT INTO dishes VALUES (NULL,%s, %s,0,1);", (dish.name, dish.img_url))
+        c.execute("INSERT INTO dishes VALUES (NULL,%s, %s,1000,1, %s);", (dish.name, dish.keywords, dish.img_url))
         db.commit()
     except:
         print("Not Ok")
@@ -57,6 +58,7 @@ def add_dish_to_db(dish):
 def getDatas(row):
     try:
         if len(row) > 2 and row[1] != 'url':
+
             url = row[1]
             page = urlopen(row[1])
             soup = BeautifulSoup(page, "lxml")
@@ -65,7 +67,7 @@ def getDatas(row):
             if imgElem:
                 print(imgElem["data-src"])
                 name = str(row[2]).split(' - ')
-                dish = Dish(name[0], str(imgElem["data-src"]))
+                dish = Dish(name[0], str(imgElem["data-src"]), str(row[12]))
                 print(name)
                 add_dish_to_db(dish)
                 return dish
@@ -79,20 +81,17 @@ def getDatas(row):
 if __name__ == '__main__':
 
     with open('raw_metadata.csv', 'r') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
 
-        # new_array = []
-        # count = 0
-        # for row in spamreader:
-        #     new_array.append(row)
-        #     count += 1
-        #     if count == 100:
-        #         break
+        new_array = []
+        count = 0
+        for row in spamreader:
+            new_array.append(row)
+            count += 1
+            if count == 500:
+                break
 
         p = Pool(4)
-        dishes = p.map(getDatas, spamreader)
+        dishes = p.map(getDatas, new_array)
         dishes = [i for i in dishes if i is not None]
         p.close()
-
-
-
